142482a0508b7dd00cf655dcf0f38a9ddaef8fea
sivefunc <sivefunc@tuta.io>
2025-09-22 16:02:40 -0400

    feat: log number of api_key and track number

--- src/soundsift/components/drivers/Spotiffy.py
+++ src/soundsift/components/drivers/Spotiffy.py
@@ -65,11 +65,17 @@ class SpotifyDownloader:
     @classmethod
     def get_youtube_url(cls, search_query, youtube_api_key):
         search_url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={search_query}&key={youtube_api_key}&type=video"
-        response = requests.get(search_url)
-        if response.status_code == 200:
-            results = response.json().get('items', [])
-            if results:
-                return f"https://www.youtube.com/watch?v={results[0]['id']['videoId']}"
+        try:
+            response = requests.get(search_url)
+            if response.status_code == 200:
+                results = response.json().get('items', [])
+                if results:
+                    return f"https://www.youtube.com/watch?v={results[0]['id']['videoId']}"
+            logger.error(response.json()['error']['message'])
+
+        except Exception as error:
+            pass
+
         return None
 
     @classmethod
@@ -87,6 +93,7 @@ class SpotifyDownloader:
         #logger.addHandler(ch)
 
         # Scan link type and fetch tracks accordingly
+        """
         spotify_link_type = cls.get_spotify_link_type(playlist_url)
         if spotify_link_type == "unknown"or spotify_link_type == "episode" or spotify_link_type == "artist" or spotify_link_type == "album":
             logger.debug("Invalid Spotify track or playlist link.")
@@ -99,6 +106,7 @@ class SpotifyDownloader:
         except ValueError as e:
             logger.debug(f"Invalid playlist URL: {e}")
             return
+        """
 
 
         # Fetch tracks from the playlist
@@ -109,32 +117,44 @@ class SpotifyDownloader:
                 logger.debug(f"CLIENT_ID = {client_id}")
                 logger.debug(f"CLIENT_SECRET = {client_secret}")
 
-                sp = cls.authenticate_spotify(client_id, client_secret)
-                tracks = fetch_tracks(sp, str(spotify_link_type), item_id)
+                #sp = cls.authenticate_spotify(client_id, client_secret)
+                #tracks = fetch_tracks(sp, str(spotify_link_type), item_id)
+                tracks = [
+                    {'name': "Beatufiul", 'artist': "Kmbra"},
+                    {'name': "Beatufiul", 'artist': "Kmbra"},
+                    {'name': "Beatufiul", 'artist': "Kmbra"},
+                    {'name': "Beatufiul", 'artist': "Kmbra"},
+                    {'name': "Beatufiul", 'artist': "Kmbra"},
+                ]
 
                 if not tracks:
                     logger.debug("No tracks found in the playlist. Please check the URL or playlist privacy settings.")
                     return
 
                 # Fetch YouTube URLs for each track
-                for track in tracks:
+                logger.debug("GET youtube urls of each track of the playlist")
+                for track_idx, track in enumerate(tracks):
+                    logger.debug(f"Fetch youtube url of track {track_idx+1} of {len(tracks)}")
                     for yt_idx, (youtube_api_key, *_) in enumerate(CREDENTIALS['youtube']):
                         track_search_query = f"{track['name']} {track['artist']}"
+                        logger.debug(f"GET track '{track_search_query}' with API_KEY='{youtube_api_key}' {yt_idx+1}/{len(CREDENTIALS['youtube'])}")
                         track['track_url'] = cls.get_youtube_url(track_search_query, youtube_api_key)
                         if track['track_url'] is not None:
                             CREDENTIALS['youtube'][yt_idx][1] = "success"
                             break
                         CREDENTIALS['youtube'][yt_idx][1] = "failed"
                         logger.debug(f"{youtube_api_key} failed, trying another of the pool...")
+                        print('---')
                 CREDENTIALS['spotify'][sp_idx][2] = "success"
                 break
 
             except Exception as e:
-                logger.debug(f"An error occurred in fetching tracks: {e}")
+                logger.error(f"An error occurred in fetching tracks: {e}")
                 CREDENTIALS['spotify'][sp_idx][2] = "failed"
                 return
 
-        if not tracks:
+        if not tracks or all(track['track_url'] is None for track in tracks):
+            logger.error('Failed fetching tracks from playlist')
             return
 
         # logger.debug youtube urls for each track
