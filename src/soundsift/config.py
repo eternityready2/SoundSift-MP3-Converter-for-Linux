413dabf386624e0c489425e30d3e5e67977dc8f0
sivefunc <sivefunc@tuta.io>
2025-09-22 20:27:07 -0400

    feat: spotify and yt credentials

--- src/soundsift/config.py
+++ src/soundsift/config.py
@@ -32,8 +32,12 @@ try:
         CREDENTIALS['spotify'] = cmap['not-tested'] + cmap['success'] + cmap['failed']
 
 except FileNotFoundError:
-    with open(CREDENTIALS_PATH / 'spotify-credentials.csv', 'w') as file:
-        pass
+    with (
+        open(CREDENTIALS_PATH / 'spotify-credentials.csv', 'w') as file1,
+        open(SRC_PATH / 'spotify-credentials.csv', 'r') as file2
+    ):
+        for line in file2.readlines():
+            file1.write(line)
 
 try:
     with open(CREDENTIALS_PATH / 'youtube-credentials.csv', 'r') as file:
@@ -50,5 +54,9 @@ try:
         CREDENTIALS['youtube'] = cmap['not-tested'] + cmap['success'] + cmap['failed']
 
 except FileNotFoundError:
-    with open(CREDENTIALS_PATH / 'youtube-credentials.csv', 'w') as file:
-        pass
+    with (
+        open(CREDENTIALS_PATH / 'youtube-credentials.csv', 'w') as file1,
+        open(SRC_PATH / 'youtube-credentials.csv', 'r') as file2
+    ):
+        for line in file2.readlines():
+            file1.write(line)
